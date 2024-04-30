49801dfe46d102b70a9f4cf9e3db2a00082e9824
Danil Skalyanskiy <dskalyanskkiy@gmail.com>
2024-05-01 02:00:41 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Animal
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Animal(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
+public interface IWalkable
+{
+    void Walk();
+}
+
+
+public class Dog : Animal,IWalkable
+{
+    public string Breed { get; set; }
+
+    public Dog(string name, int age, string breed) : base(name, age)
+    {
+        this.Breed = breed;
+    }
+
+    public void Walk()
+    {
+        Console.WriteLine($"{Name} гуляє");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Dog myDog = new Dog("Гідродезинтегратор",3000,"Велика");
+        myDog.Walk();
+    }
+}
